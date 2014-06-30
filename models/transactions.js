0db2e3e3d9f0b1b367cb851ff3028118298c27e1
JONG-IL, SEOK <daclouds@sk.com>
2014-06-30 16:24:49 +0900

    jslint

--- models/transactions.js
+++ models/transactions.js
@@ -1,27 +1,22 @@
+'use strict';
+
 exports.save = function (entity) {
-    var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/salt');
-    
-    var db = mongoose.connection;
+    var mongoose = require('mongoose'), db = mongoose.connect('mongodb://localhost/salt').connection;
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function callback () {
-	console.log('yay!');
-	var transactionSchema = mongoose.Schema({
+    db.once('open', function callback() {
+        console.log('yay!');
+        var transactionSchema = mongoose.Schema({ // transaction cannot has product infomation because we can't have all of partnership coorperation's products.
             'product_name': String,
             'deal_amount': Number,
             'currency-unit': String,
             'deal_date': Date
-        });
-        // transaction cannot has product infomation because we can't have all of partnership coorperation's products.
-
-        var Transaction = mongoose.model('Transaction', transactionSchema);
-        var tra = new Transaction(entity);
+        }), Transaction = mongoose.model('Transaction', transactionSchema), tra = new Transaction(entity);
         console.log(tra);
         tra.save(function (err, transaction) {
-            if (err) return console.error(err);
+            if (err) {
+                return console.error(err);
+            }
             console.log(transaction, ' saved');
         });
-
     });
-    
 };
