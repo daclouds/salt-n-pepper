8fb84a3fd0aceb37b04b162c8b50dae54a19f584
JONG-IL, SEOK <daclouds@sk.com>
2014-06-30 17:37:02 +0900

    jslint

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,14 +1,14 @@
-jQuery(function($) {
+'use strict';
+
+jQuery(function ($) {
     var productId = 1;
-    $.get('/products/' + productId, function(product) {
+    $.get('/products/' + productId, function (product) {
         console.log(product);
         $('#name').text(product.name);
         $('#price').text(product.price);
         $('#currency-unit').text(product['currency-unit']);
     });
-        
-    
-    $('#buy').click(function() {
+    $('#buy').click(function () {
         var product = {
             'name': $('#name').text(),
             'price': $('#price').text(),
@@ -16,7 +16,7 @@ jQuery(function($) {
         };
 
         console.log(product);
-        $.post('/products/purchase', { product: product }, function(res) {
+        $.post('/products/purchase', { product: product }, function (res) {
             alert(res);
         });
     });
--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,16 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 var transactionModel = require('../models/transactions');
 
 /* GET product detail */
-router.get('/form', function(req, res) {
+router.get('/form', function (req, res) {
     res.render('product', null);
 });
 
-router.get('/:productId', function(req, res) {
+router.get('/:productId', function (req, res) {
 
     /* sample product */
     var product = {
@@ -16,19 +18,17 @@ router.get('/:productId', function(req, res) {
         'name': 'The Lean Startup',
         'price': 18000,
         'currency-unit': 'won'
-    }
+    };
 
-    res.set({
-         'Content-Type': 'application/json'
-    });
+    res.set({ 'Content-Type': 'application/json' });
     res.json(product);
 });
 
-router.post('/purchase', function(req, res) {
+router.post('/purchase', function (req, res) {
     var transaction = {
-	'product_name': req.body.product['name'],
-        'deal_amount': req.body.product['price'],
-	'currency-unit': req.body.product['currency-unit'],
+        'product_name': req.body.product.name,
+        'deal_amount': req.body.product.price,
+        'currency-unit': req.body.product['currency-unit'],
         'deal_date': new Date()
     };
     transactionModel.save(transaction);
